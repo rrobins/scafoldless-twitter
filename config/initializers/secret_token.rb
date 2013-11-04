f5990897eb842b7bd1781c8767d5a9e46c46a963
Riley Robins <rileyrobins@live.ca>
2013-11-04 10:40:36 -0600

    rails added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScafoldlessTwitter::Application.config.secret_token = 'e870c5f0912816d39a9b9d21f561413adc999d8ebb67eded3a6835151d7f86502899d8ba00e93edd3ae5b1bc44da81182e7b2217006c1e59608d9216f437c3b8'
